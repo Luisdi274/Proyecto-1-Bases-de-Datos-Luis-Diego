10dfdbe3171548c5d90c6a56bc69f8e336371ed8
Luis Diego <luisdmocas@gmail.com>
2026-09-13 13:55:34 -0600

    BD: agregamos primer constraint de que solo permita 4 valores en atributo 'clase-periodicidad'

--- SQL/6- Updates,Restricciones_y_demas.sql
+++ SQL/6- Updates,Restricciones_y_demas.sql
@@ -3,3 +3,9 @@ ADD COLUMN id_ubicacion int  REFERENCES UBICACIONES(id_ubicacion),
 --un evento no necesariamente tendrá desde el inicio una ubicación definida, por lo que puede ser nullable
 ADD COLUMN id_serie int REFERENCES SERIES_EVENTOS(id_serie);
 --lo mismo con esto
+
+
+
+alter table series_eventos  add constraint entradas_clase_periodicidad check(clase_periodicidad  in('diario','semanal','mensual','personalizado'));
+--ya habiamos creado la tabla de series_eventos, ahora se le pone la restricción de que solo permita 4 posibles valores
+--se va a registrar todo en minuscula, hay diferencia entre mayusucla y minuscula
