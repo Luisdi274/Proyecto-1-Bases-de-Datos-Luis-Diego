e3853e9911342cc9a190de377abb114260858e95
Luis Diego <luisdmocas@gmail.com>
2026-09-13 14:18:56 -0600

    BD: segundo constraint para chequear que intervalo de dias sea consistente con lógico de clase periodicidad

--- SQL/6- Updates,Restricciones_y_demas.sql
+++ SQL/6- Updates,Restricciones_y_demas.sql
@@ -9,3 +9,9 @@ ADD COLUMN id_serie int REFERENCES SERIES_EVENTOS(id_serie);
 alter table series_eventos  add constraint entradas_clase_periodicidad check(clase_periodicidad  in('diario','semanal','mensual','personalizado'));
 --ya habiamos creado la tabla de series_eventos, ahora se le pone la restricción de que solo permita 4 posibles valores
 --se va a registrar todo en minuscula, hay diferencia entre mayusucla y minuscula
+
+alter table series_eventos add constraint tipo_entrada_intervalo_dias check(
+(clase_periodicidad ='personalizado' and intervalo_en_dias_eventos is not null) or
+(clase_periodicidad <>'personalizado' and intervalo_en_dias_eventos is null)
+);
+--recordemos que el intervalo de dias de una serie de eventos solo se usa si su periodicidad es personalizada
