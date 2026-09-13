3244cda3980996b988deb4b1ea3cb1c68a5167ec
Luis Diego <luisdmocas@gmail.com>
2026-09-13 14:41:34 -0600

    BD: tercera restriccion, para especificar dias de series de eventos

--- SQL/6- Updates,Restricciones_y_demas.sql
+++ SQL/6- Updates,Restricciones_y_demas.sql
@@ -15,3 +15,13 @@ alter table series_eventos add constraint tipo_entrada_intervalo_dias check(
 (clase_periodicidad <>'personalizado' and intervalo_en_dias_eventos is null)
 );
 --recordemos que el intervalo de dias de una serie de eventos solo se usa si su periodicidad es personalizada
+
+
+alter table series_eventos add constraint valores_para_dias_especificos_serie_evento check (
+(reserva_dias_especificos is null and clase_periodicidad ='personalizado' ) or (reserva_dias_especificos is not null and clase_periodicidad ='semanal') or
+(reserva_dias_especificos is null and clase_periodicidad = 'diario') or (reserva_dias_especificos is null and clase_periodicidad='mensual')
+
+);
+
+
+
