4836d6d20d7d4bfdf1935de1330fa510a3ba80ef
Luis Diego <luisdmocas@gmail.com>
2026-09-14 09:50:30 -0600

    BD: faltaba un END IF en una linea

--- SQL/2-scriptBD_agenda.sql
+++ SQL/2-scriptBD_agenda.sql
@@ -95,6 +95,7 @@ DECLARE
 BEGIN
 	IF NEW.id_categoria_padre IS NULL THEN
 		RETURN NEW;
+	END IF;
 --se agrega esta parte,pues si la categoria que se quiere registra no tiene un padre, pues se agrega sin problema, pues es la raiz general
     IF NEW.id_categoria_padre = NEW.id_categoria THEN
         RAISE EXCEPTION 'Una categoría no puede ser padre de sí misma.';
